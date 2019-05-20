2690722db25232d98ebd1109feb75e054ad94cb6
Mike Ryan <mikeryan56@gmail.com>
2019-05-19 20:40:34 -0400

    after running black

--- app/tests/test_all.py
+++ app/tests/test_all.py
@@ -5,17 +5,23 @@ import time
 import unittest
 from unittest.mock import patch
 import pytest
-from com_lib.file_processing import open_csv, open_json, save_csv, save_json, create_sample_files, get_data_directory_list
+from com_lib.file_processing import (
+    open_csv,
+    open_json,
+    save_csv,
+    save_json,
+    create_sample_files,
+    get_data_directory_list,
+)
 
 
 class test_file_processing(unittest.TestCase):
-
     def test_create_sample_files(self):
-        filename = 'test_sample'
-        samplesize =  1000
+        filename = "test_sample"
+        samplesize = 1000
         create_sample_files(filename, samplesize)
         result = open_csv(filename)
-        assert(len(result) == samplesize - 1)
+        assert len(result) == samplesize - 1
 
     def test_save_json(self):
         sample_dict = {"name": "bob", "date": str(datetime.datetime.now())}
@@ -26,8 +32,7 @@ class test_file_processing(unittest.TestCase):
             json_data.append(sample_dict)
 
         result = save_json(file_named, json_data)
-        assert(result == "complete")
-
+        assert result == "complete"
 
     def test_save_csv(self):
         csv_data = []
@@ -43,38 +48,34 @@ class test_file_processing(unittest.TestCase):
             csv_data.append(sample_dict)
 
             result = save_csv(file_named, csv_data)
-            assert(result == "complete")
-
+            assert result == "complete"
 
     def test_open_json(self):
         file_named = "test_1"
         result = open_json(file_named)
-        assert(len(result) > 1)
-
+        assert len(result) > 1
 
     def test_open_json_no_file(self):
         file_named = "no_file_name"
         result = open_json(file_named)
-        assert(result["error"].startswith("ERROR"))
-
+        assert result["error"].startswith("ERROR")
 
     def test_open_csv(self):
         file_named = "test_1"
         result = open_csv(file_named)
-        assert(len(result) > 1)
-
+        assert len(result) > 1
 
     def test_open_json_no_file(self):
         file_named = "no_file_name"
         result = open_json(file_named)
-        assert(result["error"].startswith("ERROR"))
+        assert result["error"].startswith("ERROR")
 
     def test_get_data_directory_json(self):
-        directory = 'json'
+        directory = "json"
         result = get_data_directory_list(directory)
-        assert(f'test_1.{directory}' in result)
+        assert f"test_1.{directory}" in result
 
     def test_get_data_directory_csv(self):
-        directory = 'csv'
+        directory = "csv"
         result = get_data_directory_list(directory)
-        assert(f'test_1.{directory}' in result)
+        assert f"test_1.{directory}" in result
