83613de320179c9e3de5ac0e14b6304f5ecf57bd
Mike Ryan <mikeryan56@gmail.com>
2019-06-01 08:10:27 -0400

    updates to basic logging configuration

--- app/com_lib/logging_config.py
+++ app/com_lib/logging_config.py
@@ -1,15 +1,13 @@
 from loguru import logger
 from pathlib import Path
 
-
 def config_logging():
     log_path = Path.cwd().joinpath("log").joinpath("app_log.log")
     logger.add(
         log_path,
         format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-        rotation="10 MB",
-        retention="10 days",
         enqueue=True,
         backtrace=False,
-        compression="zip"
-    )
+        rotation="10 MB",
+        retention="30 days",
+        compression="zip",)
