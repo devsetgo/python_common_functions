442d1e8d4d3ecd60562635e64f1ee2f5756baec1
Mike Ryan <mikeryan56@gmail.com>
2019-06-22 23:26:30 -0400

    additional examples

--- app/example.py
+++ app/example.py
@@ -1,6 +1,6 @@
 """ An example of functions in the common library"""
 import time
-from datetime import datetime
+from datetime import datetime, date
 from pathlib import Path
 from com_lib.file_functions import (
     open_json,
@@ -12,6 +12,7 @@ from com_lib.folder_functions import (
     get_directory_list,
     last_data_files_changed,
     make_folder,
+    remove_folder
 )
 
 from com_lib.logging_config import config_logging
@@ -23,15 +24,24 @@ config_logging()
 
 
 def call_folder_functions():
+    print('dir list')
     dir_list_func()
-    dir_create()
-    last_change()
+    # print('dir create')
+    # dir_create()
+    print('make dir')
     make_dir()
+    print('last change')
+    last_change()
+    # for i in range(1):
+    #     print('make dir')
+    #     make_dir()
+    #     print('delete folder')
+    #     delete_dir()
 
 
 def dir_list_func():
     dir_list = get_directory_list("data")
-    print(dir_list)
+    # print(dir_list)
     for i in dir_list:
         x = str(i)
         if "csv" in x:
@@ -43,7 +53,7 @@ def dir_list_func():
     time.sleep(5)
 
     dir_list = get_directory_list("data")
-    print(dir_list)
+    # print(dir_list)
     for i in dir_list:
         x = str(i)
         if "cs_v" in x:
@@ -52,30 +62,53 @@ def dir_list_func():
             x = p.rename(name)
 
 
-def dir_create():
-    d = datetime.now().strftime("%Y-%H-%M-%S")
-    print(d)
-    directory_to__files: str = "data"
-    file_directory = f"{directory_to__files}/{d}"
-    directory_path = Path.cwd().joinpath(file_directory)
+# def dir_create():
+#     d = datetime.now().strftime("%Y-%H-%M-%S")
+#     # print(d)
+#     directory_to__files: str = "data"
+#     file_directory = f"{directory_to__files}/{d}"
+#     directory_path = Path.cwd().joinpath(file_directory)
 
 
 def last_change():
     directory_to__files: str = "data"
     file_directory = f"{directory_to__files}"
     directory_path = Path.cwd().joinpath(file_directory)
-    last_data_files_changed(directory_path)
+    time_stamp, file_path = last_data_files_changed(directory_path)
+    # print(time_stamp, file_path)
+    
+    if '2019' in str(file_path):
+        print('yes its there')
 
 
 def make_dir():
-    d = datetime.now().strftime("%Y-%H-%M-%S")
-    print(d)
-    directory_to__files: str = "data"
-    file_directory = f"{directory_to__files}/{d}"
-    directory_path = Path.cwd().joinpath(file_directory)
-
-    for i in range(2):
+    print('wait make')
+    time.sleep(2)
+    for i in range(1):
+        
+        d = datetime.now().strftime("%Y-%M-%H-%M-%S-%f")
+        # print(d)
+        directory_to__files: str = "data"
+        file_directory = f"{directory_to__files}/x-{d}"
+        directory_path = Path.cwd().joinpath(file_directory)
         make_folder(directory_path)
+        print(directory_path.is_dir())
+
+def delete_dir():
+    print('wait delete')
+    time.sleep(2)
+    date_object = date.today()
+    # get year from date object
+    year = date_object.strftime("%Y")
+
+    file_list = get_directory_list("data")
+    # print(file_list)
+    for i in file_list:
+        # time.sleep(1)
+        f = str(i)
+        if year in f:
+            # delete directory
+            remove_folder(i)
 
 
 def get_data():
@@ -83,7 +116,8 @@ def get_data():
     try:
         result: dict = open_json(filename)
     except Exception as e:
-        print(e)
+        # print(e)
+        logger.error(e)
 
 
 def make_sample():
@@ -92,7 +126,8 @@ def make_sample():
     try:
         result = create_sample_files(filename, sample_size)
     except Exception as e:
-        print(e)
+        # print(e)
+        logger.error(e)
 
 
 def dir_list():
@@ -111,11 +146,18 @@ def dir_list():
                 count += 1
             print(count)
     except Exception as e:
-        print(e)
+        # print(e)
+        logger.error(e)
 
 
 if __name__ == "__main__":
+    print('get data')
     get_data()
+    print('make sample')
     make_sample()
+    print('dir list')
     dir_list()
+    print('folder functions')
     call_folder_functions()
+    # print('delete folder')
+    # delete_dir()
