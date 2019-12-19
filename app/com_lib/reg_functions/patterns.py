f0b4033263ae0abe14744fbbd0fed69c26062e9b
mike ryan <mikeryan56@gmail.com>
2019-12-19 17:48:20 -0500

    adding regex pattern

--- app/com_lib/reg_functions/patterns.py
+++ app/com_lib/reg_functions/patterns.py
@@ -5,19 +5,27 @@ from loguru import logger
 def pattern_between(
     text_string: str, left_character: str, right_character: str, split_character: str
 ) -> dict:
-
-    esc_text = re.escape(text_string)
-    esc_left_char = re.escape(left_character)
-    esc_right_char = re.escape(right_character)
-    esc_split_char = re.escape(split_character)
-
-    pattern = f".*{esc_left_char}(.*){esc_right_char}.*"
-
-    text_items = esc_text.split(esc_split_char)
-
-    pattern_list: list = []
+    lr_list = [left_character,right_character]
 
     try:
+        # escape input strings if necessary
+        esc_text = re.escape(text_string)
+        esc_left_char = re.escape(left_character)
+        esc_right_char = re.escape(right_character)
+        esc_split_char = re.escape(split_character)
+        # print(esc_split_char)
+
+        # regex pattern to find values between
+        pattern = f".*{esc_left_char}(.*){esc_right_char}.*"
+        # split string by a character
+        text_items = esc_text.split(esc_split_char)
+        if split_character in lr_list:
+            raise Exception
+    
+
+        # list to hold matched patterns
+        pattern_list: list = []
+
         # loop through list of items and check for pattern
         for i in text_items:
             item = re.match(pattern, i)
@@ -30,10 +38,11 @@ def pattern_between(
             "found": pattern_list,
             'matched_found': len(pattern_list),
             "pattern_parameters": {
-                "left_character": left_character,
-                "right_character": right_character,
-                "split_character": split_character,
+                "left_character": esc_left_char,
+                "right_character": esc_right_char,
+                "split_character": esc_split_char,
                 "regex_pattern": pattern,
+                "text_string": esc_text,
             },
         }
         return results
@@ -41,11 +50,31 @@ def pattern_between(
         # capture exception and return results
         results: dict = {
             "Error": e,
+            'matched_found': 0,
             "pattern_parameters": {
-                "left_character": left_character,
-                "right_character": right_character,
-                "split_character": split_character,
+                "left_character": esc_left_char,
+                "right_character": esc_right_char,
+                "split_character": esc_split_char,
                 "regex_pattern": pattern,
+                "text_string": esc_text,
+            },
+        }
+        # logging of regex error
+        logger.critical(results)
+        # return of results
+        return results
+    except Exception as e:
+        # capture exception and return results
+        e = f"the split character '{split_character}' is the same as the left '{left_character}' or right character '{right_character}'. Making this an invalid combination."
+        results: dict = {
+            "Error": e,
+            'matched_found': 0,
+            "pattern_parameters": {
+                "left_character": esc_left_char,
+                "right_character": esc_right_char,
+                "split_character": esc_split_char,
+                "regex_pattern": pattern,
+                "text_string": esc_text,
             },
         }
         # logging of regex error
