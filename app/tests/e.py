69bf3f89afd0238947bfccb5e638e2759af22dbf
mike ryan <mikeryan56@gmail.com>
2020-01-03 20:00:08 -0500

    removing unused code

--- app/tests/e.py
+++ app/tests/e.py
@@ -24,20 +24,15 @@ def run_ascii():
     for l in tqdm(char_list, desc="left char", leave=False, ascii=True):
         for r in tqdm(char_list, desc="right char", leave=False, ascii=True):
             for s in tqdm(char_list, desc="split char", leave=False, ascii=True):
-                # time.sleep(.001)
+                """# time.sleep(.001)"""
                 loop_char = [l, r]
-                # print(f'l:{l}, r:{r}, s:{s}')
                 if s not in loop_char:
                     text = f"{s}{l}found one{r}{s}{s}{l}found two{r}"
                     data = pattern_between(text, l, r, s)
-                    # data = call_pattern(l,r,s)
                     count += 1
 
                     if "Error" in data:
                         err_list.append(data)
-
-    # tasks = [non_async_function(0.1) for _ in range(10)]
-    # print([task.result() for task in tasks])
 
     return err_list, char_list, count
 
@@ -50,27 +45,19 @@ def run_ascii_two():
         char = c["Symbol"]
         if char.isprintable() == True:
             char_list.append(char)
-    # print(len(char_list))
+
     err_list = []
     count = 0
 
     for l in tqdm(char_list, desc="left char", leave=False, ascii=True):
         for r in tqdm(char_list, desc="right char", leave=False, ascii=True):
-            # for s in tqdm(char_list, desc="split char", leave=False, ascii=True):
-            # time.sleep(.0005)
-            # loop_char = [l, r]
-            # print(f'l:{l}, r:{r}, s:{s}')
-            # if s not in loop_char:
+
             text = f"{l}found one{r} {l}found two{r}"
             data = pattern_between_two_char(text, l, r)
-            # data = call_pattern(l,r,s)
-            # count += 1
 
             if "Error" in data:
                 err_list.append(data)
 
-    # tasks = [non_async_function(0.1) for _ in range(10)]
-    # print([task.result() for task in tasks])
 
     return err_list, char_list, count
 
@@ -79,7 +66,7 @@ if __name__ == "__main__":
     t0 = time.time()
     err_list, char_list, count = run_ascii_two()
     t1 = time.time() - t0
-    combinations = len(char_list) * len(char_list)  # * len(char_list)
+    combinations = len(char_list) * len(char_list)
     print(
         f"process took {t1:.2f} seconds with {combinations:,} combinations and a count cycle of {count:,}"
     )
